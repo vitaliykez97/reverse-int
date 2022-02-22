9affbff1063e585efa680fe337f231dd8df4e368
Vitaliy Kozak <96038573+vitaliykez97@users.noreply.github.com>
2022-02-22 21:29:28 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        var nStr = (n * (-1)).toString()
+    } else { var nStr = n.toString() }
+    var result = ''    
+    for (let i = nStr.length; i>0; i--){
+        result = `${result}${nStr[i-1]}`
+    }
+    return result 
 }
