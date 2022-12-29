44c03e220467026e790916b698824e5e280c6656
vitaliykez97 <frybee3@gmail.com>
2022-12-29 14:57:28 +0300

    feat:end

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-module.exports = function reverse (n) {
-    if (n < 0){
-        var nStr = (n * (-1)).toString()
-    } else { var nStr = n.toString() }
-    var result = ''    
-    for (let i = nStr.length; i>0; i--){
-        result = `${result}${nStr[i-1]}`
-    }
-    return result 
-}
+module.exports = function reverse(n) {
+    var x = Math.abs(n).toString().split("").reverse().join("");
+    console.log(n);
+    console.log(typeof x);
+    return Math.abs(n).toString().split("").reverse().join("");
+};
