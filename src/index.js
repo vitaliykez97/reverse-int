6df0856e19618435e6450c4bfd4e2dd4f2c66202
vitaliykez97 <frybee3@gmail.com>
2022-12-29 14:59:46 +0300

    fix: fix index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function reverse(n) {
-    var x = Math.abs(n).toString().split("").reverse().join("");
-    console.log(n);
-    console.log(typeof x);
-    return Math.abs(n).toString().split("").reverse().join("");
+    return Math.abs(n).toString().split("").reverse().join("").toNumber();
 };
